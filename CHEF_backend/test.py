78280fedbe3ab7929b2a933245b9e0fba7a1db68
Benjamin Lam <benjaminmlam03@gmail.com>
2025-04-26 01:06:44 -0700

    Hide key.py

--- CHEF_backend/test.py
+++ CHEF_backend/test.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyCzy5thLA2G1bcOJucI0SOIhN1LCbLNfiU")
+genai.configure(api_key="---")
 models = genai.list_models()
 for m in models:
     print(m.name)
